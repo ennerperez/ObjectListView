c5e55b328b94f9bd3f752f6ee788858cdcc9398b
Enner Pérez <ennerperez@gmail.com>
2020-05-25 22:33:53 -0500

    Dependency Fix

--- src/.files/AssemblyInfo.Version.cs
+++ src/.files/AssemblyInfo.Version.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.2.*")]
-[assembly: AssemblyFileVersion("2.9.2")]
-[assembly: AssemblyInformationalVersion("2.9.2")]
+[assembly: AssemblyVersion("2.9.3.*")]
+[assembly: AssemblyFileVersion("2.9.3")]
+[assembly: AssemblyInformationalVersion("2.9.3")]
 [assembly: System.CLSCompliant(true)]
