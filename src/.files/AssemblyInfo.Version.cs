6d02617d3e1dd56ae6d53d8c21984269a420d9fd
Enner Pérez <ennerperez@gmail.com>
2020-05-25 21:44:33 -0500

    Initial integration
    Base Code +Cake Build + NuGet Build

--- src/.files/AssemblyInfo.Version.cs
+++ src/.files/AssemblyInfo.Version.cs
@@ -1,4 +1,15 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.9.2.*")]
+[assembly: AssemblyFileVersion("2.9.2")]
+[assembly: AssemblyInformationalVersion("2.9.2")]
+[assembly: System.CLSCompliant(true)]
---
+++ src/ObjectListView/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ObjectListView")]
+[assembly: AssemblyDescription("A much easier to use ListView and friends")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bright Ideas Software")]
+[assembly: AssemblyProduct("ObjectListView")]
+[assembly: AssemblyCopyright("Copyright ©  2006-2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef28c7a8-77ae-442d-abc3-bb023fa31e57")]
